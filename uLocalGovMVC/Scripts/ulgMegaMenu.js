294a708052b0ba819499658248b2bc4e6d0b9e71
kevin jump <kevin@thejumps.co.uk>
2013-08-07 10:30:04 +0100

    megaMenu
    Finished the javaScript - not Code Dependancy, but its used on all pages
    so it's in design template.

---
+++ uLocalGovMVC/Scripts/ulgMegaMenu.js
@@ -0,0 +1,24 @@
+﻿/* basic mega menu behaviour */
+
+$(function () {
+
+    function addMega() {
+        $(this).find(".menu-drop").stop().fadeTo(75, 1).show();
+    }
+
+    function removeMega() {
+        $(this).find(".menu-drop").stop().fadeTo(75, 0, function () {
+            $(this).hide();
+        });
+    }
+
+    var megaConfig = {
+        interval: 100,
+        sensitivity: 1.25,
+        over: addMega,
+        timeout: 500,
+        out: removeMega
+    }
+
+    $("li.mega-item").hoverIntent(megaConfig);
+});
