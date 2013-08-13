0df36debe58abe48d1f04a23f5bcdd56067ff5cf
Marc Goodson <marc@snowmoon.co.uk>
2013-08-13 23:23:59 +0100

    RSS Feed For news
    god knows if that's a valid RSS feed but it's a start, uses a surface
    controller, could move to partial view, but feels right to return an
    RSSFeedActionResult from MVC ? RSSFeed at /ulgLatestNewsRssFeed using
    alttemplate

---
+++ uLocalGovMVC/Controllers/SurfaceControllers/FeedsSurfaceController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using Umbraco.Core;
+using System.Web.Mvc;
+using System.ServiceModel.Syndication;
+using uLocalGovMVC.mvc;
+
+namespace uLocalGovMVC.Controllers.SurfaceControllers
+{
+    public class FeedsSurfaceController : SurfaceController
+    {
+        public ActionResult NewsRssFeed()
+        {
+            var latestNews = Umbraco.TypedContentAtXPath("//ulgNewsItem").Where(x => x.IsVisible()).OrderByDescending(x => x.GetPropertyValue<DateTime>("articleDate")).Take(20);
+  
+          
+            var lastUpdate = (from news in latestNews
+                              select news.UpdateDate).Max();
+            SyndicationFeed feed =
+                     new SyndicationFeed("Local Government Starter Kit News RSS Feed Example",
+                                         "Latest News for Local Government Starter Kit site",
+                                         new Uri("http://www.uLocalGov.co.uk"),
+                                         "uLocalCovNews",
+                                         lastUpdate);
+
+
+            List<SyndicationItem> items = new List<SyndicationItem>();
+            foreach (var newsItem in latestNews)
+            {
+                string itemTitle = string.Format("{0}", newsItem.GetPropertyValue("title"));
+                   Uri itemURI = new Uri(newsItem.UrlWithDomain());
+                string itemContent = string.Format("<p>{0:dddd, MM yyyy} - {1}</p>", newsItem.GetPropertyValue<DateTime>("articleDate"), newsItem.GetPropertyValue("newsSummary"));
+                SyndicationItem item =
+                    new SyndicationItem(itemTitle,
+                                        itemContent,
+                                        itemURI,
+                                        newsItem.Id.ToString(),
+                                        newsItem.UpdateDate);
+
+                items.Add(item);
+            }
+            feed.Items = items;
+            return new RssActionResult(feed);
+        }
+    }
+
+    }
---
+++ uLocalGovMVC/mvc/RssActionResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace uLocalGovMVC.mvc
+{
+    public class RssActionResult : ActionResult
+    {
+         public RssActionResult()
+        {
+
+        }
+        public RssActionResult(SyndicationFeed _feed)
+        {
+            this.Feed = _feed;
+        }
+        public SyndicationFeed Feed { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.ContentType = "application/rss+xml";
+
+            Rss20FeedFormatter rssFormatter = new Rss20FeedFormatter(Feed);
+            using (XmlWriter writer = XmlWriter.Create(context.HttpContext.Response.Output))
+            {
+                rssFormatter.WriteTo(writer);
+            }
+        }
+    }
+    
+}
